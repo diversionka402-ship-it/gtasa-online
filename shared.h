5f1ae5417d64352ffd33379e9285b2b08d4e818d
diversionka402-ship-it <diversionka402@gmail.com>
2026-09-03 17:01:39 +0300

    Update shared.h

--- shared.h
+++ shared.h
@@ -3,7 +3,9 @@
 // Пакет, который отправляется по сети
 struct PlayerData {
     int playerId;
+    char name[32]; // Имя игрока
     float x;
     float y;
     float z;
+    float rotation; // Куда смотрит игрок (угол поворота)
 };
